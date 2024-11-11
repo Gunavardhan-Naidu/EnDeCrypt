b838153a6f80134deae8719a7d8cf693b954da5d
Gunavardhan <gunavardhan1966@gmail.com>
2024-11-11 18:24:12 +0530

    Deleting all the default stuff and making application basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,10 @@
-import './App.css';
 import React from 'react';
-// import Navbar from './components/Navbar';
-// import EncryptionForm from './components/EncryptionForm';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Navbar /> */}
-        <p>
-          <h1>EnDecrypt</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://gunavardhan-naidu.github.io/EnDeCrypt/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          App link
-        </a>
-        {/* <EncryptionForm /> */}
-      </header>
+    <div>
+        <Navbar />
     </div>
   );
 }
