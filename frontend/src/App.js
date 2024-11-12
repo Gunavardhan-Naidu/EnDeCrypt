087298fade8090dd1e738ffc5d08473d820b6481
Gunavardhan <gunavardhan1966@gmail.com>
2024-11-13 04:58:03 +0530

    small changes1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
         <Navbar />
+        <h1>Hello World!</h1>
     </div>
   );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,11 +9,11 @@ function header() {
   return (
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand >Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Features</Nav.Link>
+            <Nav.Link>Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
